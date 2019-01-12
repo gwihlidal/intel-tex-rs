308daeeb14e04e92f4eb0a699678d299198f2fd0
Graham Wihlidal <graham@wihlidal.ca>
2019-01-12 03:05:31 +0100

    Match ISPC settings to vendor version

--- build.rs
+++ build.rs
@@ -1,10 +1,16 @@
 extern crate ispc;
 
+/*
+    ISPC project file builds the kernels as such:
+    <Command Condition="'$(Configuration)|$(Platform)'=='Release|x64'">ispc -O2 "%(Filename).ispc" -o "$(TargetDir)%(Filename).obj" -h "$(ProjectDir)%(Filename)_ispc.h" --target=sse2,sse4,avx,avx2 --opt=fast-math</Command>
+    <Outputs Condition="'$(Configuration)|$(Platform)'=='Release|x64'">$(TargetDir)%(Filename).obj;$(TargetDir)%(Filename)_sse2.obj;$(TargetDir)%(Filename)_sse4.obj;$(TargetDir)%(Filename)_avx.obj;$(TargetDir)%(Filename)_avx2.obj;</Outputs>
+*/
+
 fn compile_kernel() {
     let mut cfg = ispc::Config::new();
     cfg.file("vendor/ISPC Texture Compressor/ispc_texcomp/kernel.ispc");
-    cfg.opt_level(3);
-    //cfg.optimization_opt(ispc::OptimizationOpt::FastMath);
+    cfg.opt_level(2);
+    cfg.optimization_opt(ispc::opt::OptimizationOpt::FastMath);
     //cfg.quiet();
     cfg.woff();
     //cfg.instrument();
@@ -14,8 +20,8 @@ fn compile_kernel() {
 fn compile_kernel_astc() {
     let mut cfg = ispc::Config::new();
     cfg.file("vendor/ISPC Texture Compressor/ispc_texcomp/kernel_astc.ispc");
-    cfg.opt_level(3);
-    //cfg.optimization_opt(ispc::OptimizationOpt::FastMath);
+    cfg.opt_level(2);
+    cfg.optimization_opt(ispc::opt::OptimizationOpt::FastMath);
     //cfg.quiet();
     cfg.woff();
     //cfg.instrument();
